b6de07107b8deaa012bda51c2c2b3bab132b8571
vantjnh1991 <nguyenvantinh.cnktmtk3@gmail.com>
2020-01-31 19:40:30 +0700

    remove config

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 9 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#9 SMP PREEMPT Fri Jan 31 10:58:32 +07 2020"
-#define LINUX_COMPILE_BY "cutin2018"
-#define LINUX_COMPILE_HOST "cutin2018"
-#define LINUX_COMPILER "Android (4691093 based on r316199) clang version 6.0.2 (https://android.googlesource.com/toolchain/clang 183abd29fc496f55536e7d904e0abae47888fc7f) (https://android.googlesource.com/toolchain/llvm 34361f192e41ed6e4e8f9aca80a4ea7e9856f327) (based on LLVM 6.0.2svn)"
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.14.113-VNKernel.S10-5G.Stable-2"
